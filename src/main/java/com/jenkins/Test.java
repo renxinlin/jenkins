e7c7e2c3e4e6d6b0d69b18538dd4c85d179ff71f
renxinlin <2284057845@qq.com>
2019-01-19 15:50:53 +0800

    init jenkins project

---
+++ src/main/java/com/jenkins/Test.java
@@ -0,0 +1,11 @@
+package com.jenkins;
+
+/**
+ * author: renxl
+ * date: 20190119
+ */
+class Test {
+    public static void main(String[] args) {
+        System.out.println("----------hello jenkinis----------");
+    }
+}
